f7bdcaefa41fa0f394c91ccf75d274b8949fd860
badrulhanif <hanifbaharuddin@gmail.com>
2022-06-28 20:09:01 +0800

    Update UI

--- pages/movie.js
+++ pages/movie.js
@@ -7,9 +7,9 @@ import { useRouter } from "next/dist/client/router";
 function MovieTitle() {
   const router = useRouter();
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen md:flex-row text-center p-16">
+    <div className="flex flex-col items-center justify-center min-h-screen p-16 text-center md:flex-row">
       <div>
-        <h1 className="text-3xl font-bold mb-5">
+        <h1 className="mb-5 text-3xl font-bold">
           The Umbrella Academy <a className="text-red-500"> 3</a>
         </h1>
         <Image
@@ -18,10 +18,10 @@ function MovieTitle() {
           width={256.4}
           height={320}
         />
-        <p className="text-red-500 mt-1 text-medium">
+        <p className="mt-1 text-red-500 text-medium">
           Only 2 seats available. (You & Me)
         </p>
-        <div className="mt-2 px-12">
+        <div className="px-12 mt-2">
           <div className="pr-64">
             <button className="rounded border-solid py-0.5 px-2 mt-3 mr-2 bg-black text-xs text-white cursor-default">
               PG-13
@@ -33,7 +33,7 @@ function MovieTitle() {
               83% Rotten Tomatoes
             </button>
           </div>
-          <p className="flex items-center justify-center text-medium mt-2 px-96 text-justify">
+          <p className="flex items-center justify-center mt-2 text-justify text-medium px-96">
             This supposed to be the movie descriptions but no way...it is too
             long to write it here and this is like the best sci-fi Netflix
             series kot, 83% on rotten tomatoes. You not gonna miss it and come
@@ -41,10 +41,10 @@ function MovieTitle() {
           </p>
         </div>
         <button
-          className="rounded-md border-solid py-1 px-4 justify-end bg-cyan-500 mt-6 text-white"
+          className="justify-end px-4 py-1 mt-6 text-white border-solid rounded-md bg-cyan-500"
           onClick={() => router.push("/watchMovie")}
         >
-          Let's Watch
+          Lets Watch
         </button>
       </div>
     </div>
--- pages/watchMovie.js
+++ pages/watchMovie.js
@@ -1,7 +1,7 @@
 function AddON() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center md:flex-row">
-      WooHoo! Let's go watch teh movie.
+      WooHoo! Lets go watch teh movie.
     </div>
   );
 }
