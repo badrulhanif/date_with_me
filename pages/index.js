d6ceecd4b7283d4a4b8bbec1d952d06622c75b1d
badrulhanif <103894805+badrulhanif@users.noreply.github.com>
2023-03-01 11:47:12 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ function MovieTitle() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen md:flex-row text-center text-4xl">
       <div>
-        Hey,
+        Hey, wanna watch
         <p className="flex items-center justify-center font-bold">
           The Umbrella Academy <p className="text-red-500 ml-2.5"> 3</p>
         </p>
